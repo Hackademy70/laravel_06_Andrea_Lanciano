8f6ac919c77ccd472da55818ba331e775622ca32
Andrea Lanciano <andrealanciano@me.com>
2023-04-24 22:15:28 +0200

    aggiustato form di modifica per adattarlo al nuovo tipo di articolo

--- resources/views/singlecard/edit.blade.php
+++ resources/views/singlecard/edit.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="title">Modify this Article!</x-slot>
 
     <div class="container">
-        <div class="row justify-content-center mt-5">
+        <div class="row justify-content-center my-5">
             <div class="col-8">
                 {{-- Form --}}
                 <form method="post" action="{{ route('article.update', $article) }}" enctype="multipart/form-data">
@@ -12,18 +12,33 @@
                         <label for="title" class="form-label">Title</label>
                         <input type="text" class="form-control" name="title" value="{{ $article->title }}">
                     </div>
+                    {{-- primo paragrafo --}}
                     <div class="mb-3">
                         <label for="subtitle" class="form-label">SubTitle</label>
                         <input type="text" class="form-control" name="subtitle" value="{{ $article->subtitle }}">
                     </div>
-                    <label for="article">Write your article</label>
+                    <label for="paragraph">Modify first paragraph</label>
                     <div class="form-floating">
-                        <textarea class="form-control" placeholder="Leave a comment here" name="article" style="height: 300px">{{ $article->article }}</textarea>
+                        <textarea class="form-control" placeholder="Leave a comment here" name="paragraph" style="height: 300px">{{ $article->paragraph }}</textarea>
                       </div>
                       <label for="img" class="form-label mt-2">Cover</label>
                       <div class="mb-3 col-4 d-flex w-100">
                         <input type="file" class="form-control" name="img">
-                        <img src="{{ Storage::url($article->img) }}" alt=" {{ $article->title }} " style="width: 80px;">
+                        <img src="{{ Storage::url($article->img) }}" alt=" {{ $article->subtitle }} " style="width: 80px;">
+                    </div>
+                    {{-- secondo paragrafo --}}
+                    <div class="mb-3">
+                        <label for="subtitle2" class="form-label">SubTitle</label>
+                        <input type="text" class="form-control" name="subtitle2" value="{{ $article->subtitle2 }}">
+                    </div>
+                    <label for="paragraph2">Modify second paragraph</label>
+                    <div class="form-floating">
+                        <textarea class="form-control" placeholder="Leave a comment here" name="paragraph2" style="height: 300px">{{ $article->paragraph2 }}</textarea>
+                      </div>
+                      <label for="img" class="form-label mt-2">Cover</label>
+                      <div class="mb-3 col-4 d-flex w-100">
+                        <input type="file" class="form-control" name="img">
+                        <img src="{{ Storage::url($article->img2) }}" alt=" {{ $article->subtitle2 }} " style="width: 80px;">
                     </div>
                     <div class="mb-3 mt-1">
                         <label for="author" class="form-label">Author</label>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,35 +13,35 @@
     {{-- prova layout --}}
 
     <div class="band mt-3">
-    @if ($firstArticle != null)
-        <div class="item-1">
-            <div class="my-card">
-                <div class="thumb" style="background-image: url({{ Storage::url($firstArticle->img) }});"></div>
-                <article class="my-article">
-                    <h2>{!! Str::limit($firstArticle->title, 45) !!}</h2>
-                    <p class="my-card-paragraph">{!! Str::limit($firstArticle->paragraph, 200) !!}</p>
-                    <span class="author">{{ $firstArticle->author }}</span>
-                    <div class="d-flex mt-3">
-                        <a href="{{ route('singlecard.show', ['article' => $firstArticle]) }}" class="btn btn-primary">Read More</a>
-                        <a href="{{ route('article.edit', ['article' => $firstArticle]) }}" class="btn btn-warning mx-3"><i class="fa-solid fa-pen-to-square"></i></a>
-                        <form method="post" action="{{ route('article.delete', ['article' => $firstArticle]) }}">
-                            @method('DELETE')
-                            @csrf
-                            <button class="btn btn-danger">
-                                <i class="fa-solid fa-bucket"></i>
-                            </button>
-                        </form>
-                    </div>
-                </article>
+        @if ($firstArticle != null)
+            <div class="item-1">
+                <div class="my-card">
+                    <div class="thumb" style="background-image: url({{ Storage::url($firstArticle->img) }});"></div>
+                    <article class="my-article">
+                        <h2>{!! Str::limit($firstArticle->title, 45) !!}</h2>
+                        <p class="my-card-paragraph">{!! Str::limit($firstArticle->paragraph, 200) !!}</p>
+                        <span class="author">{{ $firstArticle->author }}</span>
+                        <div class="d-flex mt-3">
+                            <a href="{{ route('singlecard.show', ['article' => $firstArticle]) }}" class="btn btn-primary">Read More</a>
+                            <a href="{{ route('article.edit', ['article' => $firstArticle]) }}" class="btn btn-warning mx-3"><i class="fa-solid fa-pen-to-square"></i></a>
+                            <form method="post" action="{{ route('article.delete', ['article' => $firstArticle]) }}">
+                                @method('DELETE')
+                                @csrf
+                                <button class="btn btn-danger">
+                                    <i class="fa-solid fa-bucket"></i>
+                                </button>
+                            </form>
+                        </div>
+                    </article>
+                </div>
             </div>
-        </div>
-    @endif
-        @foreach ($articles as $article)
-        @if ($firstArticle->id != $article->id)
-        <x-card
-            :article="$article"
-        /> 
         @endif
+        @foreach ($articles as $article)
+            @if ($firstArticle->id != $article->id)
+                <x-card
+                    :article="$article"
+                /> 
+            @endif
         @endforeach
     </div>
 
